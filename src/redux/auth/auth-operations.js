0b4f1821c5ca1974e7409bf4f6144472ee95eb04
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-10-24 13:47:51 +0300

    logOut fixed

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -3,9 +3,20 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
+//ставим общий заголовок в запросах axios
+const token = {
+    set(token) {
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    },
+    unset() {
+        axios.defaults.headers.common.Authorization = '';
+    }
+}
+
 export const register = createAsyncThunk('auth/register', async credentials => {
     try {
         const { data } = await axios.post('/users/signup', credentials);
+        token.set(data.token);
         return data;
     } catch (error) {
 
@@ -15,15 +26,17 @@ export const register = createAsyncThunk('auth/register', async credentials => {
 export const logIn = createAsyncThunk('auth/login', async credentials => {
     try {
         const { data } = await axios.post('/users/login', credentials);
+        token.set(data.token);
         return data;
     } catch (error) {
 
     }
 });
 
-export const logOut = createAsyncThunk('auth/login', async credentials => {
+export const logOut = createAsyncThunk('auth/logout', async credentials => {
     try {
-        const { data } = await axios.post('/users/login', credentials);
+        const { data } = await axios.post('/users/logout', credentials);
+        token.unset();
         return data;
     } catch (error) {
 
--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
-import {register, logIn} from './auth-operations';
+import {register, logIn, logOut} from './auth-operations';
 
 const initialState = {
-    user: { name: null, email: null, password:null },
+    user: { name: null, email: null },
     token: null,
     isLoggedIn: false,
 }
@@ -21,7 +21,12 @@ const authSlice = createSlice({
             state.user = action.payload.user;
             state.token = action.payload.token;
             state.isLoggedIn = true;
-        }
+        },
+        [logOut.fulfilled](state, action) {
+            state.user = { name: null, email: null };
+            state.token = null;
+            state.isLoggedIn = false;
+        },
     },
 });
 
